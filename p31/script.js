9861fd2fd11ec3bc2ab63af97c01631597d05b7f
Rascal0902 <geonho050902@gmail.com>
2024-05-16 13:01:27 +0900

    commit

--- p31/script.js
+++ p31/script.js
@@ -1,1 +1,6 @@
-const isValidNumber = () => {}; /* TODO */
+const isValidNumber = (target) => {
+    return Number.isInteger(parseFloat(target)) && parseFloat(target)<10 && parseFloat(target)>0;
+};
+
+const test = isValidNumber("4.1");
+console.log(test)
--- p32/script.js
+++ p32/script.js
@@ -1,1 +1,18 @@
-const getDivisors = () => {}; /* TODO */
+const getDivisors = (num) => {
+    const arr=[];
+    for(let i=1; i<Math.sqrt(num); i++){
+        if (num%i==0) {arr.push(i); arr.push(num/i);}
+    }
+    if(Math.sqrt(num)%1.0 == 0){
+        arr.push(Math.sqrt(num));
+    }
+    arr.sort(function(a, b)  {
+        if(a > b) return 1;
+        if(a === b) return 0;
+        if(a < b) return -1;
+      });
+    return arr;
+}; 
+
+const divisor = getDivisors(4);
+console.log(divisor);
