6d9f992058470fd1cbdef445fe58620493c6907b
leapin-lizards <wonjaechoi2@gmail.com>
2024-05-24 17:10:02 +0900

    week5

--- p31/script.js
+++ p31/script.js
@@ -1,1 +1,7 @@
-const isValidNumber = () => {}; /* TODO */
+const isValidNumber = (inp) => {
+    inp=Number(inp)
+    if(inp>=1&&inp<=9&&Number.isInteger(inp))
+        return true;
+    else
+        return false;
+};
--- p32/script.js
+++ p32/script.js
@@ -1,1 +1,13 @@
-const getDivisors = () => {}; /* TODO */
+const getDivisors = (inp) => {
+    let res=[];
+    for(let i=1;i<=Math.sqrt(inp);i++){
+        if(inp%i==0){
+            res.push(i);
+            res.push(inp/i)
+        }
+    }      
+    res.sort((a, b)=> {return a - b;});
+    res=new Set(res);
+    res=Array.from(res);
+    return res;
+};
