c8e97ad89f5b0993302a53b1332120d63141ca72
undercurrent1962 <undercurrent111@gmail.com>
2024-05-26 23:58:29 +0900

    Basics of JS Exercises

--- p31/script.js
+++ p31/script.js
@@ -1,1 +1,14 @@
-const isValidNumber = () => {}; /* TODO */
+const isValidNumber = (input) => {
+
+    const number = Number(input);
+    const integers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+
+    if (!isFinite(number) || isNaN(number)) {
+        return false;
+    }
+    if (integers.includes(number)) {
+        return true;
+    }
+    return false;
+};
--- p32/script.js
+++ p32/script.js
@@ -1,1 +1,13 @@
-const getDivisors = () => {}; /* TODO */
+const getDivisors = (num) => {
+    let divisors = [];
+    let sqrtNum = Math.sqrt(num);
+    for (let i = 1; i <= sqrtNum; i++) {
+        if (num % i === 0) {
+            divisors.push(i);
+            if (i !== num / i) {
+                divisors.push(num / i);
+            }
+        }
+    }
+    return divisors.sort((a, b) => a - b);
+};
