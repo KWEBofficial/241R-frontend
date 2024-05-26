b03f4197c11e2e904054afd4e54dc6f5d5df07be
TaeHyunShin <sth3678@korea.ac.kr>
2024-05-26 10:57:41 +0900

    feat: fifth assignment

--- p32/script.js
+++ p32/script.js
@@ -1,1 +1,13 @@
-const getDivisors = () => {}; /* TODO */
+const getDivisors = (x) => {
+    let d=[];
+    for(let i=1;i<=Math.sqrt(x);++i)
+        {
+            if(i*i==x)
+            {
+                d.push(i);
+                break;
+            }
+            if(x%i==0)d.push(i,x/i);
+        }
+        return d.sort((a,b)=>a-b);
+};
