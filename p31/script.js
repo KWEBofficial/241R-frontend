4f89ad3a96caa9069111ab3d72c5136e9e2f42c6
serin4103 <serin4103@gmail.com>
2024-05-26 23:56:11 +0900

    ..

--- p31/script.js
+++ p31/script.js
@@ -1,1 +1,5 @@
-const isValidNumber = () => {}; /* TODO */
+const isValidNumber = (x) => {
+    const validNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    if(x in validNumber) return true;
+    return false;
+};
--- p32/script.js
+++ p32/script.js
@@ -1,1 +1,10 @@
-const getDivisors = () => {}; /* TODO */
+const getDivisors = (x) => {
+    let divisors = [1, x];
+    const sqrtX = Math.sqrt(x);
+    for(let i = 2; i < sqrtX; i++) {
+        if(x%i === 0) {
+            divisors = divisors.concat([i, x/i]);
+        }
+    }
+    return divisors.sort((first, second) => first - second);
+};
