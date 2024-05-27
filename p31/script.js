c78d6d721ba16210ed652b2a116a44772f52305d
Skytree <skytree2002@naver.com>
2024-05-27 11:33:10 +0900

    hw5

--- p31/script.js
+++ p31/script.js
@@ -1,1 +1,10 @@
-const isValidNumber = () => {}; /* TODO */
+const isValidNumber = (input) => {
+    let num = Number(input);
+
+    if (Number.isInteger(num) && num >= 1 && num <= 9){
+        return true;
+    }
+    else{
+        return false;
+    }
+}; /* TODO */
--- p32/script.js
+++ p32/script.js
@@ -1,1 +1,16 @@
-const getDivisors = () => {}; /* TODO */
+const getDivisors = (input) => {
+    let divs = []
+
+    for (let i = 1; i <= Math.sqrt(input); i++){
+        if (input % i === 0) {
+            divs.push(i);
+            if (input / i !== i) {
+                divs.push(input / i);
+            }
+        }
+    }
+
+    divs.sort((a, b) => a - b);
+
+    return divs;
+}; /* TODO */
