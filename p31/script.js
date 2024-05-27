ee8af4bc9324ebe94a315bb5be39aa29277aa946
minjun <mj-kevin@hanmail.net>
2024-05-27 23:09:26 +0900

    fifth assignment

--- p31/script.js
+++ p31/script.js
@@ -1,1 +1,9 @@
-const isValidNumber = () => {}; /* TODO */
+const isValidNumber = (x) => {
+    
+    if (1 <= x && x <= 9 && Number.isInteger(x)){
+        return true;
+    }  
+    else{
+        return false;
+    }
+}; /* TODO */
--- p32/script.js
+++ p32/script.js
@@ -1,1 +1,13 @@
-const getDivisors = () => {}; /* TODO */
+const getDivisors = (x) => {
+    const returnlst = [];
+    const root = Math.floor(Math.sqrt(x));
+    for(let i = 1; i <= root ; i++){
+        if(x % i == 0){
+            returnlst.push(i);
+            if(i**2 !== x){
+                returnlst.push(x / i)
+            }   
+        }
+    }
+    return returnlst.sort((a, b) => a-b);
+}; /* TODO */
