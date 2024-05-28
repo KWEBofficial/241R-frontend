837182dcb74f327ed827be8d87c476faca3cec4e
thunnie920 <tjdms030920@naver.com>
2024-05-28 18:16:53 +0900

    5주차 과제 다시 제출합니다

--- p31/script.js
+++ p31/script.js
@@ -1,5 +1,5 @@
 const isValidNumber = (number) => { 
-    if (number > 0 && number < 10) {
+    if (number > 0 && number < 10 && number % 1 === 0) {
         return true;
     } else {
         return false;
