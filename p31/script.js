5f6933ecf42d8b3f64bc9c0b94d5f5c65277217d
starligdt777 <165145955+starligdt777@users.noreply.github.com>
2024-05-28 17:10:56 +0900

    Update script.js

--- p31/script.js
+++ p31/script.js
@@ -1,2 +1,8 @@
-const set = new Set([1,2,3,4,5,6,7,8,9])
-const isValidNumber = (x) => {return set.has(x)};
+const arr = [1,2,3,4,5,6,7,8,9]
+const isValidNumber = (x) => {
+    if(x===true) return false;
+    for(var num of arr){
+        if(x==num) return true;
+    }
+    return false;
+}
