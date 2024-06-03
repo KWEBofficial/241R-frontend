8d9d8382616d7651da8f585f6101a9dbe194ff61
HanGyeol Kim <jerry020935@gmail.com>
2024-06-03 18:32:37 +0900

    5주차 과제 수정

--- p32/script.js
+++ p32/script.js
@@ -2,7 +2,11 @@ const getDivisors = (x) => {
     var n = Math.sqrt(x);
     var head = [];
     var tail = [];
-    for (var i=1; i<n; i++) {
+    for (var i=1; i<=n; i++) {
+        if (i == n) {
+            head.push(i)
+            break
+        }
         if (x%i == 0) {
             head.push(i);
             tail.unshift(x/i);
