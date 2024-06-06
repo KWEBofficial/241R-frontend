252b489ed92d89b8cc6380370d5bfede767a84d2
hansung-ku <hansungk@korea.ac.kr>
2024-06-07 01:24:00 +0900

    Assignment05 change

--- p33/script.js
+++ p33/script.js
@@ -1,16 +1,7 @@
 const ellipse = {
   width: 10,
   height: 5,
+  getArea: function () { return this.width * this.height * Math.PI },
+  getPerimeter: function () { return 2*Math.PI*Math.sqrt((this.width*this.width+this.height*this.height)/2) },
+  getEccentricity: function () { return Math.sqrt(1 - (this.height/this.width)**2) }
 };
-
-ellipse.getArea = function () {
-  return this.width * this.height * Math.PI
-}
-
-ellipse.getPerimeter = function () {
-  return 2*Math.PI*Math.sqrt((this.width*this.width+this.height*this.height)/2)
-}
-
-ellipse.getEccentricity = function () {
-  return Math.sqrt(1 - (this.height/this.width)**2)
-}
