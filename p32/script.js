b10b798a2c27312a6a385ffb383d5952a0a0f4e1
onff02 <yetpi0413@naver.com>
2024-06-24 21:49:57 +0900

    change

--- p32/script.js
+++ p32/script.js
@@ -1,11 +1,11 @@
 const getDivisors = (num) => {
     let arr = [];
 
-    for(let i=1; i<=sqrt.max(num); i++) {
+    for(let i=1; i<=Math.sqrt(num); i++) {
         if(num%i === 0){
             arr.push(i);
             if(num/i!== i){
-                arr.push(i);
+                arr.push(num/i);
             }    
         }
     }
