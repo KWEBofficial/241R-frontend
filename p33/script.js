f6ea9decffc666c481b3304c02ef91321544a74c
W1nyu <khan3617@naver.com>
2024-06-25 14:06:23 +0900

    5

--- p33/script.js
+++ p33/script.js
@@ -1,13 +1,7 @@
 const ellipse = {
   width: 10,
-  height: 5
+  height: 5,
+  getArea: function() { console.log(Math.PI * this.width * this.height)},
+  getPerimeter: function() { console.log(2 * Math.PI * Math.sqrt((this.width ** 2 + this.height ** 2) / 2))},
+  getEccentricity: function() { console.log(Math.sqrt(1 - ((this.height / this.width) ** 2)))}
 };
-ellipse.getArea = function() {
-  return Math.PI * this.width * this.height;
-}
-ellipse.getPerimeter = function() {
-return 2 * Math.PI * Math.sqrt((this.width ** 2 + this.height ** 2) / 2);
-}
-ellipse.getEccentricity = function() {
-  return Math.sqrt(1 - ((this.height / this.width) ** 2));
-}
